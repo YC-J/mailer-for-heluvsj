71fb941960081c105bbfefb602fc4f6a87dec3fc
jo <fantasticjyc@gmail.com>
2019-03-11 18:56:44 +0900

    Fix cors

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -2,6 +2,9 @@ module V1
   class ContactsController < ApplicationController
     def create
       ::Contacts::Mailer.contact_from(params[:name], params[:email], params[:content]).deliver
+      render json: { redirect_url: '/thanks' }, status: :ok
+    rescue => e
+      render json: { errors: e.message }, status: :unprocessable_entity
     end
   end
 end
